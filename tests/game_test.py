5d0482c3d334246b480eadfaabf4d180700e5160
Schwarzrock, Noah (SE-A/32) <noah.schwarzrock@volkswagen.de>
2024-10-26 15:02:36 +0200

    Added tests for the function: define property.

--- tests/game_test.py
+++ tests/game_test.py
@@ -24,6 +24,21 @@ class GameTest(unittest.TestCase):
         with self.assertRaises(GameException) as context:
             self.game.define_card("Sauron")
         self.assertEqual(str(context.exception), "Error! Card Sauron is already defined.")
-    
+        
+    def test_define_card_property_success_1(self):
+        """Testet, ob eine Eigenschaft richtig erzeugt wurde."""
+        self.game.define_property("power", int)
+        self.assertIn("power", self.game.properties)
+        self.assertEqual(self.game.properties["power"], {"type": int})
+        
+    def test_define_card_property_duplicate_1(self):
+        """Testet, ob eine Eigenschaft richtig erzeugt wurde."""
+        self.game.define_property("power", int)
+        with self.assertRaises(GameException) as context:
+            self.game.define_property("power", int)
+        self.assertEqual(str(context.exception), "Error! property power is already defined.")
+        
+        
+        
 if __name__ == "__main__":
     unittest.main()
