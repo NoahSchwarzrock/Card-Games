b60e67fab1e3a5cbfbeda922aa8861a48cf7e0a6
Schwarzrock, Noah (SE-A/32) <noah.schwarzrock@volkswagen.de>
2024-10-26 14:04:39 +0200

    Added tests for the function: define_card

---
+++ tests/game_test.py
@@ -0,0 +1,25 @@
+import unittest
+from my_game import MyGame
+from game_exception import GameException
+
+class GameTest(unittest.TestCase):
+    
+    def setUp(self):
+        """Initilisiert die MyGame-Instanz vor jedem Test."""
+        self.game = MyGame()
+    
+    def test_define_card_success_1(self):
+        """Testet, ob eine Karte mit dem richtigen Namen erstellt wurde."""
+        self.game.define_card("Sauron")
+        self.assertIn("Sauron", self.game.cards)
+        self.assertEqual(self.game.cards["Sauron"], {})
+
+    def test_define_card_duplicate_1(self):
+        """Testet, ob bereits eine Karte mit demselben Namen besteht und die Exception geworfen wird."""
+        self.game.define_card("Sauron")
+        with self.assertRaises(GameException) as context:
+            self.game.define_card("Sauron")
+        self.assertEqual(str(context.exception), "Card 'Sauron' is already defined.")
+    
+if __name__ == "__main__":
+    unittest.main()
