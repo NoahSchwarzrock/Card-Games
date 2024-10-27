b45525b9d8d9155df8df42b04f7333fd011f5aaa
Schwarzrock, Noah (SE-A/32) <noah.schwarzrock@volkswagen.de>
2024-10-27 14:33:41 +0100

    Changed the test-docstrings to english language.

--- tests/game_test.py
+++ tests/game_test.py
@@ -9,30 +9,30 @@ from game_exception import GameException
 class GameTest(unittest.TestCase):
     
     def setUp(self):
-        """Initilisiert die MyGame-Instanz vor jedem Test."""
+        """Initializes the MyGame instance before each test."""
         self.game = MyGame()
     
     def test_define_card_success_1(self):
-        """Testet, ob eine Karte mit dem richtigen Namen erstellt wurde."""
+        """Tests if a card is created with the correct name."""
         self.game.define_card("Sauron")
         self.assertIn("Sauron", self.game.cards)
         self.assertEqual(self.game.cards["Sauron"], {})
 
     def test_define_card_duplicate_1(self):
-        """Testet, ob bereits eine Karte mit demselben Namen besteht und die Exception geworfen wird."""
+        """Tests if a card with the same name already exists and raises an exception."""
         self.game.define_card("Sauron")
         with self.assertRaises(GameException) as context:
             self.game.define_card("Sauron")
         self.assertEqual(str(context.exception), "Error! Card Sauron is already defined.")
         
     def test_define_card_property_success_1(self):
-        """Testet, ob eine Eigenschaft richtig erzeugt wurde."""
+        """Tests if a property is created correctly."""
         self.game.define_property("power", int)
         self.assertIn("power", self.game.properties)
         self.assertEqual(self.game.properties["power"], {"type": int})
         
     def test_define_card_property_duplicate_1(self):
-        """Testet, ob eine Eigenschaft richtig erzeugt wurde."""
+        """Tests if a property with the same name already exists and raises an exception."""
         self.game.define_property("power", int)
         with self.assertRaises(GameException) as context:
             self.game.define_property("power", int)
